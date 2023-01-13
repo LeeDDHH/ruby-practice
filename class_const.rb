069a290b2b09ee44cd4d54d1d06aba0282f6e5e4
DongHeon Lee <niche3600@gmail.com>
2023-01-13 13:30:46 +0900

    クラス定数練習

---
+++ class_const.rb
@@ -0,0 +1,30 @@
+class Car
+  REGION = 'USA'
+  @@count = 0
+  def initialize(name)
+    @name = name
+    @@count += 1
+  end
+  def hello
+    puts "Hello! I am #{@name}"
+    puts "#{@@count} instanse(s)"
+  end
+
+  def self.info
+    puts "#{@@count} instance(s). Region: #{REGION}"
+  end
+end
+
+kitt = Car.new('Kitt')
+# kitt.hello
+Car.info
+
+karr = Car.new('Karr')
+# karr.hello
+Car.info
+
+nakamura = Car.new('nakamura')
+# nakamura.hello
+Car.info
+
+puts Car::REGION
