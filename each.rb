f4788ac08de3c9d82dca1c7f26406d6f9df29e9e
DongHeon Lee <niche3600@gmail.com>
2023-01-13 12:48:08 +0900

    each練習

---
+++ each.rb
@@ -0,0 +1,15 @@
+# numbers = [1,2,3,4,5]
+# numbers.each do |number|
+#   puts number
+# end
+
+# numbers.each {|number|
+#   puts number
+# }
+
+# numbers.each { |number| puts number }
+
+colors = ['red', 'green', 'blue']
+colors.each do |color|
+  puts color
+end
