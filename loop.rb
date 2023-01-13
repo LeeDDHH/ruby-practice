6c39ddd5ed4409a21886ebde338c46d57a53a0cf
DongHeon Lee <niche3600@gmail.com>
2023-01-13 13:08:12 +0900

    ループ処理中のnext練習

--- loop.rb
+++ loop.rb
@@ -1,6 +1,12 @@
-i = 0
-loop do
-  puts i
-  i += 1
-  break if i == 10
+# i = 0
+# loop do
+#   puts i
+#   i += 1
+#   break if i == 10
+# end
+
+numbers = [1,2,3,4,5]
+numbers.each do |n|
+  next if n % 2 == 0
+  puts n
 end
